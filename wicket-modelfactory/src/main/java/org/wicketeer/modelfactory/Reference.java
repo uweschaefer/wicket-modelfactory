68014a69f81481d45af79602ef0b34e8d60a3e57
Uwe Schäfer <uweschaefer@thomas-daily.de>
2012-08-13 11:54:15 +0200

    name change

---
+++ wicket-modelfactory/src/main/java/org/wicketeer/modelfactory/Reference.java
@@ -0,0 +1,43 @@
+//
+//
+// Copyright 2012-2012 Uwe Schäfer <uwe@codesmell.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package org.wicketeer.modelfactory;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+class Reference
+{
+    private final Object object;
+    private final Exception invokationPath;
+
+    Reference(final Object o)
+    {
+        object = checkNotNull(o);
+        invokationPath = new Exception();
+    }
+
+    Object getObject()
+    {
+        return object;
+    }
+
+    Exception getInvokationPath()
+    {
+        return invokationPath;
+    }
+
+}
